2cf65ecc17dc633e7ae5e6bf2aac6231fc3629c7
Daniel Flanigan <daniel.flanigan@cea.fr>
2018-02-26 17:43:53 +0100

    Added setup.py, requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Paramp',
+      version='0.1',
+      description='Code for analyzing parametric amplifiers',
+      author='Greg Ward',
+      author_email='daniel.flanigan@cea.fr',
+      url='github.com/danielflanigan/paramp',
+      packages=['paramp'],
+     )
